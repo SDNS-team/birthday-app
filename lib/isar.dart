2f59fbc5b767583d52ee6c7737da826e10a8b053
Svetozar <swetozar.volkov@yandex.ru>
2022-01-09 22:16:40 +0300

    add isar initializer

---
+++ lib/isar.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:isar/isar.dart';
+import 'package:isar_connect/isar_connect.dart';
+import 'package:path_provider/path_provider.dart';
+
+late final Isar isar;
+
+Future<void> initializeIzar() async {
+  if (kDebugMode) {
+    initializeIsarConnect();
+  }
+
+  Directory? dir;
+
+  try {
+    dir = await getTemporaryDirectory();
+  } catch (ex) {
+    print('fuck this');
+  } finally {
+    dir ??= Directory.current;
+  }
+
+  isar = await Isar.open(
+    schemas: [],
+    directory: dir.path,
+  );
+}
